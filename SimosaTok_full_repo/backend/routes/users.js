80af4ea3e613c5cea1c6bc1ab49739feb2a5a41a
washalofficial <washal.official1@gmail.com>
2025-11-28 12:47:48 +0500

    Delete SimosaTok_full_repo/backend directory

--- SimosaTok_full_repo/backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// placeholder user routes for extension
-router.get('/me', (req, res) => {
-  res.json({ message: 'user route placeholder' });
-});
-
-module.exports = router;
