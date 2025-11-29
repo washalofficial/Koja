f4d486ad897d87db1e1817829c2b1b3bd155bc98
washalofficial <washal.official1@gmail.com>
2025-11-29 11:30:18 +0500

    feat: add initial application and comments drawer components

---
+++ screens/HomeScreen.js
@@ -0,0 +1,5 @@
+// This file was removed because it contained React Native code incompatible with the web platform.
+// The functionality has been replaced by components/VideoFeed.tsx
+export default function HomeScreen() {
+  return null;
+}
